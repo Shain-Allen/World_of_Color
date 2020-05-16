22a11b1ea8237d6dfa18891acecf466c013f0785
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-05-15 22:38:56 -0700

    more input system progress

--- World of Color/Assets/Scripts/Player_Movement.cs
+++ World of Color/Assets/Scripts/Player_Movement.cs
@@ -5,5 +5,29 @@ using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
-    
+    PlayerControlls input;
+
+    //move
+    Vector2 movementInput;
+
+    private void Awake()
+    {
+        input = new PlayerControlls();
+        input.Player.Movement.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
 }

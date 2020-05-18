726127f31f10b8eb0cf6fca913a5267a3575f86c
Conor Brennan <conorpb@outlook.com>
2020-05-17 20:22:45 -0700

    main menu
    basic menu navigation

---
+++ World of Color/Assets/Scripts/Menu_Controller.cs
@@ -0,0 +1,21 @@
+﻿//Menu_Controller.cs
+//By: Conor Brennan
+//Last Edited: 5/17/2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour
+{
+
+    public void Next_Scene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

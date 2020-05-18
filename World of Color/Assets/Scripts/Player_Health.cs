279e7eef07e1d32f0d2352d82db9151384b54aba
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-05-18 14:49:24 -0700

    Player Shader is finished

--- World of Color/Assets/Scripts/Player_Health.cs
+++ World of Color/Assets/Scripts/Player_Health.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Player_Health : MonoBehaviour
 {
+    //health UI 
     public int health;
     public int numOfHearts;
-
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite EmptyHeart;
 
+    //player graidiant Control
+    public Material PlayerMat;
+    float distance;
+
     private void Update()
     {
-        if(health > numOfHearts)
+        HealthUIController();
+        PlayerMatController();
+    }
+
+
+    void HealthUIController()
+    {
+        if (health > numOfHearts)
         {
             health = numOfHearts;
         }
@@ -31,7 +43,7 @@ public class Player_Health : MonoBehaviour
                 hearts[i].sprite = EmptyHeart;
             }
 
-            if(i < numOfHearts)
+            if (i < numOfHearts)
             {
                 hearts[i].enabled = true;
             }
@@ -40,5 +52,11 @@ public class Player_Health : MonoBehaviour
                 hearts[i].enabled = false;
             }
         }
+    }
+
+    void PlayerMatController()
+    {
+        distance = (0.5f / numOfHearts) * health + 0.06f;
+        PlayerMat.SetFloat("Distance", distance);
     }
 }

3457b12754cde11f07e40503828d7634f7583454
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-05-28 14:05:18 -0700

    Sound

--- World of Color/Assets/Scripts/Room_Trigger.cs
+++ World of Color/Assets/Scripts/Room_Trigger.cs
@@ -3,11 +3,18 @@
 //Last Edited: 5/21/2020
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
+using Unity.Mathematics;
+using System;
 
 public class Room_Trigger : MonoBehaviour
 {
     public GameObject virtualCamera;
+
+    public AudioMixer audioMixer;
+    public StartingAreaManager areaManager;
+    public int[] enemiesToAudioTracks = new int[5];
 
     //if the player enters a room, that room's vcam is turned on
     private void OnTriggerEnter2D(Collider2D collision)
@@ -15,6 +22,12 @@ public class Room_Trigger : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             virtualCamera.SetActive(true);
+            Console.WriteLine("Muted");
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Bass1", 0.1f, 0f));
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Harmony", 0.1f, 0f));
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Drums", 0.1f, 0f));
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Bass2", 0.1f, 0f));
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "MelodicLine", 0.1f, 0f));
         }
     }
 
@@ -25,6 +38,59 @@ public class Room_Trigger : MonoBehaviour
         {
             virtualCamera.SetActive(false);
         }
+    }
+    
+    private void Update()
+    {
+        //unmute audio in this order
+        
+        //1. Bass1
+        //2. Harmony
+        //3. Drums
+        //4. Bass2
+        //5. MelodicLine
+        
+    
+        if (areaManager.purifiedMonsters >= enemiesToAudioTracks[0])
+        {
+            Console.WriteLine("unmuted");
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Bass1", 1f, 1f));
+        }
+        if (areaManager.purifiedMonsters >= enemiesToAudioTracks[1])
+        {
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Harmony", 1f, 1f));
+        }
+        if (areaManager.purifiedMonsters >= enemiesToAudioTracks[2])
+        {
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Drums", 1f, 1f));
+        }
+        if (areaManager.purifiedMonsters >= enemiesToAudioTracks[3])
+        {
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "Bass2", 1f, 1f));
+        }
+        if (areaManager.purifiedMonsters >= enemiesToAudioTracks[4])
+        {
+            StartCoroutine(Room_Trigger.StartFade(audioMixer, "MelodicLine", 1f, 1f));
+        }
+    
+    }
+        
+    public static IEnumerator StartFade(AudioMixer audioMixer, string exposedParam, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float currentVol;
+        audioMixer.GetFloat(exposedParam, out currentVol);
+        currentVol = Mathf.Pow(10, currentVol / 20);
+        float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
+            audioMixer.SetFloat(exposedParam, Mathf.Log(newVol) * 20);
+            yield return null;
+        }
+        yield break;
     }
 
 }

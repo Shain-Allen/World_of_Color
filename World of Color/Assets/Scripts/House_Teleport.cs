3f0dcb76e14087bfa7121ef7d191fe6dc734112f
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-05-28 16:07:54 -0700

    player Idle right

---
+++ World of Color/Assets/Scripts/House_Teleport.cs
@@ -0,0 +1,20 @@
+﻿//House_Teleport.cs
+//By: Conor Brennan
+//Last Edited: 5/28/2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House_Teleport : MonoBehaviour
+{
+    public GameObject teleportLocation;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            player.transform.position = teleportLocation.transform.position;
+        }
+    }
+}
